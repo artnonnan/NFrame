d80e74126585c50ce6a14a8d0348f3a1f54ad879
Nontanan <nontanan@orcsoft.co.th>
2025-08-09 16:00:21 +0700

    add upload uniform mat4

--- SandBox/src/SandBoxMain.cpp
+++ SandBox/src/SandBoxMain.cpp
@@ -98,9 +98,11 @@ public:
             #version 330 core
             layout(location = 0) out vec4 color;
 
+            uniform vec4 u_Color;
+
             void main()
             {
-                color = vec4(0.2, 0.3, 0.8, 1.0); 
+                color = u_Color; 
             }
         )";
         m_BlueShader.reset(new NFrame::Shader(vertexSrc2, fragmentSrc2));
@@ -136,12 +138,19 @@ public:
         NFrame::Renderer::BeginScene(m_Camera);
 
         glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+        glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+        glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
         for(int y = 0; y < 20; y++)
         {
             for(int x = 0; x < 20; x++){
 
             glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
             glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+            if(x % 2 == 0) {
+                m_BlueShader->UploadUniformFloat4("u_Color", redColor);
+            } else {
+                m_BlueShader->UploadUniformFloat4("u_Color", blueColor);
+            }
 
             NFrame::Renderer::Submit(m_BlueShader, m_squareVA, transform);
             }
